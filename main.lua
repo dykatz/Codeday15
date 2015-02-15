6fa5d144f1122464526693704906b57bca34537a
MustachedNinja <kostya.yatsuk9@live.com>
2015-02-15 02:49:27 -0800

    I did stuff
    my second commit

--- main.lua
+++ main.lua
@@ -12,9 +12,25 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.print("press escape to exit", 500, 535)
+	love.graphics.print("Start Wave", 65, 535)
+	love.graphics.rectangle('line', 50, 520, 100, 50)
 	myMap:draw()
 end
 
 function love.mousepressed(x, y, b)
-	myMap:onClick(x, y)
+	myMap:onClick(x, y)	
+	if (love.mouse.isDown('r')) then
+		x,y = love.mouse.getPosition()
+		if (x<150 and x>50) then
+			if (y<570 and y>520) then
+				myMap:startWave()
+			end
+		end
+	end
+end
+function love.keypressed(key)
+   if key == "escape" then
+      love.event.quit()
+   end
 end
